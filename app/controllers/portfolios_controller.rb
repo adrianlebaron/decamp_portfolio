8058dc33c9ef61611da83374bbff8edd2e96bdf7
Adrian Lebaron <adrianlebaron@gmail.com>
2018-06-27 13:46:48 -0600

    Implemented image defaults for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -16,12 +16,13 @@ class PortfoliosController < ApplicationController
 
     respond_to do |format|
     if @portfolio_item.save
-       format.html { redirect_to@portfolio_path, notice: 'Your portfolio item is now live.' }
+       format.html { redirect_to@portfolios_path, notice: 'Your portfolio item is now live.' }
       else
         format.html { render :new }
       end
     end
   end
+
   def edit
     @portfolio_item = Portfolio.find(params[:id])
   end
@@ -30,7 +31,7 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.find(params[:id])
     respond_to do |format|
       if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
-        format.html { redirect_to @portfolios_path, notice: 'The record successfully updated.' }
+        format.html { redirect_to@portfolios_path, notice: 'The record successfully updated.' }
       else
         format.html { render :edit }
       end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180615204142) do
+ActiveRecord::Schema.define(version: 20180627175439) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,8 +21,8 @@ ActiveRecord::Schema.define(version: 20180615204142) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "slug"
-    t.integer "status", default: 0
     t.bigint "topic_id"
+    t.integer "status", default: 0
     t.index ["slug"], name: "index_blogs_on_slug", unique: true
     t.index ["topic_id"], name: "index_blogs_on_topic_id"
   end
