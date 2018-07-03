fb51397dc1fd71cf1d42771ca75e4ae1b60181e5
Adrian Lebaron <adrianlebaron@gmail.com>
2018-07-03 09:06:45 -0600

    debugged a few things from all the portfolios could not get nested atributes to work

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -9,19 +9,20 @@ class PortfoliosController < ApplicationController
 
    def new
       @portfolio_item = Portfolio.new
+      3.times { @portfolio_item.technologies.build }
    end
   
-  def create
-   @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+    def create
+     @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name]))
 
-    respond_to do |format|
-    if @portfolio_item.save
-       format.html { redirect_to@portfolios_path, notice: 'Your portfolio item is now live.' }
-      else
-        format.html { render :new }
+      respond_to do |format|
+       if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
+        else
+         format.html { render :new }
+        end
       end
     end
-  end
 
   def edit
     @portfolio_item = Portfolio.find(params[:id])
@@ -30,8 +31,8 @@ class PortfoliosController < ApplicationController
   def update
     @portfolio_item = Portfolio.find(params[:id])
     respond_to do |format|
-      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
-        format.html { redirect_to@portfolios_path, notice: 'The record successfully updated.' }
+     if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
       else
         format.html { render :edit }
       end
--- db/seeds.rb
+++ db/seeds.rb
@@ -50,6 +50,7 @@ puts "9 portfolio items created"
 3.times do |technology|
   Portfolio.last.technologies.create!(
     name: "Technology #{technology}"
-   )
+    )
 end
-puts "3 tecnologies created"
+
+puts "3 technologies created"
